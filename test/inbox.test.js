a1ed15fd30c846cb7bb9e1a849e0494ca96f9827
utkc137-codeverse <utkarsh.learn2020@gmail.com>
2020-07-16 18:28:53 +0530

    #

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,3 +1,26 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+class Car {
+    park(){
+        return 'Stopped';
+    }
+
+    drive(){
+        return 'Vroom';
+    }
+}
+
+describe('car class', () => {
+    it('can park', () => {
+        const car = new Car();
+        assert.equal(car.park(), 'Stopped');
+    });
+
+    it('can drive', () => {
+        const car = new Car();
+        assert.equal(car.drive(), 'Vroom');
+    });
+});
